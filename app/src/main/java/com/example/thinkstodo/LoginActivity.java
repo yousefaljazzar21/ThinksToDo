749aa8b2e6eb7a820a0b0b07f01aef5ec509884d
yousefaljazzar21 <Feras.nedal@hotmail.com>
2020-12-31 22:56:12 +0200

    Add

--- app/src/main/java/com/example/thinkstodo/LoginActivity.java
+++ app/src/main/java/com/example/thinkstodo/LoginActivity.java
@@ -5,19 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
-
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
     }
 
-    public void nextBut(View view) {
-        Intent intent=new Intent(this, RegisterActivity.class);
+    public void createTest(View view) {
+        Intent intent=new Intent(this,RegisterActivity.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/thinkstodo/RegisterActivity.java
@@ -0,0 +1,55 @@
+package com.example.thinkstodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+    EditText editText1, editText2, editText3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        editText1 = findViewById(R.id.nameText);
+        editText2 = findViewById(R.id.passwordText);
+        editText3 = findViewById(R.id.emailText);
+    }
+
+    public void loginText(View view) {
+
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void bttn_create(View view) {
+        String name = editText1.getText().toString();
+        String password = editText2.getText().toString();
+        String email = editText3.getText().toString();
+        if (name.isEmpty()) {
+            editText1.setError("Please fill this filed.");
+            editText1.requestFocus();
+            return;
+        }
+        if (password.isEmpty()) {
+            editText1.setError("Please fill this filed.");
+            editText1.requestFocus();
+            return;
+        }
+        if (email.isEmpty()) {
+            editText1.setError("Please fill this filed.");
+            editText1.requestFocus();
+            return;
+        }
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
